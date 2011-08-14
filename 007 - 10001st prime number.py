c3741f60c3636cb1dfde01da040ed60d365ea31f
jsundram <jsundram@gmail.com>
2011-08-14 17:02:23 -0400

    use timing code

--- 007 - 10001st prime number.py
+++ 007 - 10001st prime number.py
@@ -9,22 +9,30 @@
 # 10,0001 = n / ln(n)
 # So, we can safely use n to be roughly 150,000.
 
-import time
-# 1) Construct a sieve
-# 2) Find the 10,0001st prime in it.
-start_time = time.time()
-UPPER_BOUND = 150000
-sieve = range(0,UPPER_BOUND)
-counter = 0
-for i in xrange(2, UPPER_BOUND):
-    if (sieve[i] != 0):
-        counter += 1 # prime!
-        if (counter == 10001):
-            print sieve[i]
-            break
-        else:
-            for j in xrange(i, UPPER_BOUND, i):
-                sieve[j] = 0
-print "Elapsed Time: ", time.time() - start_time, "second(s)"
+from timed import timed
+
+@timed
+def original_solution():
+    # 1) Construct a sieve
+    # 2) Find the 10,0001st prime in it.
+    UPPER_BOUND = 150000
+    sieve = range(0, UPPER_BOUND)
+    counter = 0
+    for i in xrange(2, UPPER_BOUND):
+        if sieve[i] != 0:
+            counter += 1 # prime!
+            if counter == 10001:
+                return sieve[i]
+            else:
+                for j in xrange(i, UPPER_BOUND, i):
+                    sieve[j] = 0
+
+def main():
+    print 'The answer (original) is: %d' % original_solution()
+
+
+if __name__ == '__main__':
+    main()
+
 
 

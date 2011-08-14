7ac5ce74818cd9fbc5ef3dc553b76ca45502ab27
jsundram <jsundram@gmail.com>
2011-08-14 16:49:41 -0400

    Add timing code, remove useless sorts, do less work, minor refactoring, add runtime

--- 003 - Prime factorization of 317584931803.py
+++ 003 - Prime factorization of 317584931803.py
@@ -6,30 +6,39 @@
 # Quadratic Sieve is probably easier to implement:
 # http://en.wikipedia.org/wiki/Quadratic_sieve#The_algorithm
 import math
-
+from timed import timed
 
 # Brain-dead approach
-K = 317584931803
-
-
 def factor(K):
     factors = []
-    for i in xrange(2, math.floor(math.sqrt(K))):
-        if (K % i == 0):
+    for i in xrange(2, int(math.sqrt(K))):
+        if K % i == 0:
             factors.append(i)
-            factors.append(K / i)
-    factors.sort()
+    print K, factors
     return factors
-    
-primes = []
-for k in factor(K):   
-    if (len(factor(k)) == 0):
-        primes.append(k)
-primes.sort()
 
-print primes[len(primes)-1]
+@timed
+def golfed(n):
+    return max(k for k in factor(n) if len(factor(k)) == 0)
+
+@timed
+def original_solution(n):
+    """runtime on mba is 80ms""" 
+    primes = []
+    for k in factor(n):   
+        if len(factor(k)) == 0:
+            primes.append(k)
+
+    print "primes:",  primes
+    return max(primes)
 
 # Runtime:
 # if n is the number of factors of the integer N
 # O(sqrt(N)/2) (to find the factors)
 # O(n*sqrt(N)) to test primality of each of them. Not really N, but something smaller than it.
+def main():
+    N = 317584931803
+    print 'The answer (original) is: %d' % original_solution(N) # golfed(N)
+
+if __name__ == '__main__':
+    main()

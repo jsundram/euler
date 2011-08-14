8fa5e4489708ac7224c17a16e752693d8fb931bd
jsundram <jsundram@gmail.com>
2011-08-14 16:45:18 -0400

    remove unused code, avoid visiting pairs already visited, start at max to minimize palindrome checks, make palindrome check shorter

--- 004 - 6-digit Palindrome.py
+++ 004 - 6-digit Palindrome.py
@@ -4,30 +4,29 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 import math
+from timed import timed
 
 ### Bounds: 317**2 > 100,000 999x999 = 998,001
-def factor(K):
-    factors = []
-    for i in xrange(2, math.floor(math.sqrt(K))):        
-        if (K % i == 0):
-            factors.append(i)
-            factors.append(K / i)
-    factors.sort()
-    return factors
 
-def isPalindrome(s):
-    if (len(s) != 6):
-        return False
+def palindrome(s):
+    """purpose-built for this problem"""
+    if len(s) == 6:
+        return s[0] == s[5] and s[1] == s[4] and s[2] == s[3]
+    
+@timed
+def original_solution():
+    best = 0;
+    for i in xrange(999, 100, -1):
+         for j in xrange(999, i, -1):
+              product = i * j
+              if best < product and palindrome(str(product)):
+                  best = product
+    return best
 
-    return (s[0] == s[5] and s[1] == s[4] and s[2] == s[3])
+def main():
+    print "Original solution is: ", original_solution()
 
-palindrome = 0;
-for i in xrange(100,999):
-     for j in xrange(100,999):
-          product = i * j
-          if (product > palindrome and isPalindrome(str(product))):
-              palindrome = product
-
-print palindrome
+if __name__ == '__main__':
+    main()
 
 #runtime: O(N)
